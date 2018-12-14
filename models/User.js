58554438684d5d230b8cf5deaa3efd68cdcd1251
hardiked <hmodi97122@yahoo.in>
2018-12-14 15:28:58 +0530

    initial commit

---
+++ models/User.js
@@ -0,0 +1,26 @@
+var mongoose=require('mongoose');
+var Schema=mongoose.Schema;
+import 'babel-polyfill';
+import * as bcrypt from 'bcryptjs';
+
+var UserSchema=new Schema({
+	email:{type:String,lowercase:true,default:null, unique: true},
+	username:{type:String, default:null, unique: true},
+	password:{type:String,default:null},
+	confirmed:{type:Boolean,default:true},
+	forgotPasswordLocked:{type:Boolean,default:false},
+	twitterId:{type:String,default:null}
+});
+
+UserSchema.pre('save',async function(next){
+	// var user=this;
+	if(this.password){
+		if(!this.isModified('password')){
+			return next();
+		}
+		this.password = await bcrypt.hash(this.password,10);
+		next();
+	}
+});
+
+module.exports=mongoose.model('User',UserSchema);

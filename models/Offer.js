a8ccd31cfefef649bf657cbed369b55b078cf362
hardikmodi1 <hmodi2457@gmail.com>
2018-12-18 16:04:32 +0530

    updating offer status mutation is ready

--- models/Offer.js
+++ models/Offer.js
@@ -5,7 +5,7 @@ import "babel-polyfill";
 var OfferSchema = new Schema({
     userId: { type: String, required: true },
     codeReviewRequestId: { type: String, required: true },
-    accepted: { type: Boolean, default: false }
+    status: { type: String, default: "inprogress" }
 });
 
 OfferSchema.index({ userId: 1, codeReviewRequestId: 1 }, { unique: true });
